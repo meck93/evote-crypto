fa4522f43e7508f15e34feb360200e8fe20f5997
nikzaugg <zauggnik@gmail.com>
2019-10-23 09:04:58 +0200

    add prettier formatting

--- src/ecc/eccElgamal.js
+++ src/ecc/eccElgamal.js
@@ -1,83 +1,83 @@
-const EC = require("elliptic").ec;
-const ec = new EC("secp256k1");
-const crypto = require("crypto");
-const BN = require("bn.js");
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+const crypto = require('crypto')
+const BN = require('bn.js')
 
 const UPPER_BOUND_RANDOM = new BN(
-  "ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe fffffc2d",
-  "hex",
-  "be"
-);
-const RAND_SIZE_BYTES = 33;
+  'ffffffff ffffffff ffffffff ffffffff ffffffff ffffffff fffffffe fffffc2d',
+  'hex',
+  'be'
+)
+const RAND_SIZE_BYTES = 33
 
 // fix constants for values 1 -> 4 and 0 -> 2
-const M_1 = ec.curve.pointFromX(4);
-const M_0 = ec.curve.pointFromX(2);
+const M_1 = ec.curve.pointFromX(4)
+const M_0 = ec.curve.pointFromX(2)
 console.log(
-  "are the chosen on the curve?",
+  'are the chosen on the curve?',
   ec.curve.validate(M_1) && ec.curve.validate(M_0)
-);
+)
 
 function getRandomPointOnCurve() {
-  let randomBytes = crypto.randomBytes(RAND_SIZE_BYTES);
-  let randomPoint = new BN(randomBytes);
+  let randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
+  let randomPoint = new BN(randomBytes)
 
   // ensure that the random value is in range [1,p-1]
   while (!randomPoint.lte(UPPER_BOUND_RANDOM)) {
-    randomBytes = crypto.randomBytes(RAND_SIZE_BYTES);
-    randomPoint = new BN(randomBytes);
+    randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
+    randomPoint = new BN(randomBytes)
   }
-  return randomPoint;
+  return randomPoint
 }
 
 function encrypt(message, pubK) {
-  const randBN = getRandomPointOnCurve();
+  const randBN = getRandomPointOnCurve()
 
   // compute c1: generator ecc-multiply randomValue
-  let c1 = ec.g.mul(randBN);
-  console.log("Is c1 on the curve?", ec.curve.validate(c1));
+  let c1 = ec.g.mul(randBN)
+  console.log('Is c1 on the curve?', ec.curve.validate(c1))
 
   // compute s: h^randomValue
   // whereby h = publicKey => h = g^privateKeyOfReceiver (h is publically available)
-  const s = pubK.mul(randBN);
-  console.log("Is point s on the curve?", ec.curve.validate(s));
+  const s = pubK.mul(randBN)
+  console.log('Is point s on the curve?', ec.curve.validate(s))
 
   // compute c2: s*message
-  const c2 = s.add(message);
-  console.log("is c2 on curve?", ec.curve.validate(c2));
+  const c2 = s.add(message)
+  console.log('is c2 on curve?', ec.curve.validate(c2))
 
-  return [c1, c2];
+  return [c1, c2]
 }
 
 function decrypt(cipherText, privK) {
-  const c1 = cipherText[0];
-  const c2 = cipherText[1];
+  const c1 = cipherText[0]
+  const c2 = cipherText[1]
 
   // compute s: c1^privateKey
-  const s = c1.mul(privK);
-  console.log("is s on the curve?", ec.curve.validate(s));
+  const s = c1.mul(privK)
+  console.log('is s on the curve?', ec.curve.validate(s))
 
   // compute s^-1: the multiplicative inverse of s (probably the most difficult)
-  let s_inverse = s.neg();
-  console.log("is s^-1 on the curve?", ec.curve.validate(s_inverse));
+  let s_inverse = s.neg()
+  console.log('is s^-1 on the curve?', ec.curve.validate(s_inverse))
 
   // compute m: c2 ecc-add s^-1
-  const m = c2.add(s_inverse);
-  console.log("is m on curve?", ec.curve.validate(m));
+  const m = c2.add(s_inverse)
+  console.log('is m on curve?', ec.curve.validate(m))
 
-  return m;
+  return m
 }
 
 function demo() {
-  const keyPair = ec.genKeyPair();
-  const privateKey = keyPair.getPrivate();
-  const publicKey = keyPair.getPublic();
+  const keyPair = ec.genKeyPair()
+  const privateKey = keyPair.getPrivate()
+  const publicKey = keyPair.getPublic()
 
-  const cipherText = encrypt(M_1, publicKey);
-  const plainText = decrypt(cipherText, privateKey);
+  const cipherText = encrypt(M_1, publicKey)
+  const plainText = decrypt(cipherText, privateKey)
 
-  console.log("are the messages the same?", plainText.eq(M_1));
-  console.log("plaintext is:", plainText.getX());
+  console.log('are the messages the same?', plainText.eq(M_1))
+  console.log('plaintext is:', plainText.getX())
 }
 
-demo();
+demo()
--- src/elgamal.ts
+++ src/elgamal.ts
@@ -1,142 +1,142 @@
-const BN = require("bn.js");
-const random = require("random");
-const printConsole = true;
+const BN = require('bn.js')
+const random = require('random')
+const printConsole = true
 
 interface PublicKey {
-  p: any; // prime
-  g: any; // generator
-  h: any;
+  p: any // prime
+  g: any // generator
+  h: any
 }
 
 interface Cipher {
-  c1: any;
-  c2: any;
+  c1: any
+  c2: any
 }
 
 const encrypt = (message: any, pk: PublicKey): Cipher => {
   // generate a random value
-  const randomValue = new BN(random.int(1, pk.p - 2), 10);
-  console.log("enc secret   (r)", randomValue);
+  const randomValue = new BN(random.int(1, pk.p - 2), 10)
+  console.log('enc secret   (r)', randomValue)
 
   // compute c1: generator^randomValue
-  let c1 = pk.g.pow(randomValue).mod(pk.p);
-  printConsole && console.log("c1\t\t", c1);
+  let c1 = pk.g.pow(randomValue).mod(pk.p)
+  printConsole && console.log('c1\t\t', c1)
 
   // compute s: h^randomValue whereby
   // h = publicKey => h = g^privateKeyOfReceiver (h is publically available)
-  const s = pk.h.pow(randomValue).mod(pk.p);
-  printConsole && console.log("s\t\t", s);
+  const s = pk.h.pow(randomValue).mod(pk.p)
+  printConsole && console.log('s\t\t', s)
 
   // compute mh: generator^message
-  const mh = pk.g.pow(message).mod(pk.p);
-  printConsole && console.log("mh\t\t", mh);
+  const mh = pk.g.pow(message).mod(pk.p)
+  printConsole && console.log('mh\t\t', mh)
 
   // compute c2: s*message_homomorphic
-  const c2 = s.mul(mh).mod(pk.p);
-  printConsole && console.log("c2\t\t", c2);
-  printConsole && console.log("------------------------");
+  const c2 = s.mul(mh).mod(pk.p)
+  printConsole && console.log('c2\t\t', c2)
+  printConsole && console.log('------------------------')
 
-  return { c1: c1, c2: c2 };
-};
+  return { c1: c1, c2: c2 }
+}
 
 const add = (em1: Cipher, em2: Cipher): Cipher => {
   return {
     c1: em1.c1.mul(em2.c1).mod(p),
-    c2: em1.c2.mul(em2.c2).mod(p)
-  };
-};
+    c2: em1.c2.mul(em2.c2).mod(p),
+  }
+}
 
 const decrypt = (cipherText: Cipher, sk: any, pk: PublicKey): any => {
-  let c1 = cipherText.c1;
-  let c2 = cipherText.c2;
+  let c1 = cipherText.c1
+  let c2 = cipherText.c2
 
   // compute s: c1^privateKey
-  let s = c1.pow(sk).mod(pk.p);
-  printConsole && console.log("s\t\t", s);
+  let s = c1.pow(sk).mod(pk.p)
+  printConsole && console.log('s\t\t', s)
 
   // compute s^-1: the multiplicative inverse of s (probably the most difficult)
-  let s_inverse = s.invm(pk.p);
-  printConsole && console.log("s^-1\t\t", s_inverse);
+  let s_inverse = s.invm(pk.p)
+  printConsole && console.log('s^-1\t\t', s_inverse)
 
   // compute m: c2 * s^-1 | c2 / s
-  let m_h = c2.mul(s_inverse).mod(pk.p);
-  printConsole && console.log("m_h\t\t", m_h);
+  let m_h = c2.mul(s_inverse).mod(pk.p)
+  printConsole && console.log('m_h\t\t', m_h)
 
   // alternative computation
   // 1. compute p-2
-  const pMinusX = pk.p.sub(new BN(2, 10));
+  const pMinusX = pk.p.sub(new BN(2, 10))
   //const pMinusX = p.sub(new BN(2, 10));
-  printConsole && console.log("p - 2\t\t", pMinusX);
+  printConsole && console.log('p - 2\t\t', pMinusX)
 
   // 2. compute pre-result s^(p-x)
-  const sPowPMinusX = s.pow(pMinusX).mod(pk.p);
-  printConsole && console.log("s^(p-x)\t\t", sPowPMinusX);
+  const sPowPMinusX = s.pow(pMinusX).mod(pk.p)
+  printConsole && console.log('s^(p-x)\t\t', sPowPMinusX)
 
   // 3. compute message - msg = c2*s^(p-x)
-  let msg_homo = c2.mul(sPowPMinusX).mod(pk.p);
-  printConsole && console.log("msg_homo\t", msg_homo);
+  let msg_homo = c2.mul(sPowPMinusX).mod(pk.p)
+  printConsole && console.log('msg_homo\t', msg_homo)
 
   // 4.
-  let m_ = new BN(1, 10);
+  let m_ = new BN(1, 10)
   while (
     !pk.g
       .pow(m_)
       .mod(pk.p)
       .eq(m_h)
   ) {
-    m_ = m_.add(new BN(1, 10));
+    m_ = m_.add(new BN(1, 10))
   }
 
-  let msg = new BN(1, 10);
+  let msg = new BN(1, 10)
   while (
     !pk.g
       .pow(msg)
       .mod(pk.p)
       .eq(msg_homo)
   ) {
-    msg = msg.add(new BN(1, 10));
+    msg = msg.add(new BN(1, 10))
   }
 
-  console.log("plaintexts\t", m_, msg);
+  console.log('plaintexts\t', m_, msg)
   console.log(
-    "are plaintexts the same?",
+    'are plaintexts the same?',
     msg.eq(message),
     msg.eq(m_),
     m_.eq(message)
-  );
-  printConsole && console.log("------------------------");
+  )
+  printConsole && console.log('------------------------')
 
-  return msg;
-};
+  return msg
+}
 
-const p = new BN(7, 10);
-const g = new BN(3, 10);
-const sk = new BN(random.int(1, p - 2), 10);
-const h = g.pow(sk).mod(p);
+const p = new BN(7, 10)
+const g = new BN(3, 10)
+const sk = new BN(random.int(1, p - 2), 10)
+const h = g.pow(sk).mod(p)
 
-const pk = { p: p, g: g, h: h };
+const pk = { p: p, g: g, h: h }
 
-const message = new BN(random.int(1, pk.p - 1), 10);
+const message = new BN(random.int(1, pk.p - 1), 10)
 for (let i = 0; i < 10; i++) {
   printConsole && console.log(i)
-  printConsole && console.log("prime      (p)\t", pk.p);
-  printConsole && console.log("generator  (g)\t", pk.g);
-  printConsole && console.log("dec secret (x)\t", sk);
-  printConsole && console.log("           (h)\t", pk.h);
-  console.log("plaintext  (m)\t", message);
-  printConsole && console.log("------------------------");
-  decrypt(encrypt(message, pk), sk, pk);
-  console.log("\n");
+  printConsole && console.log('prime      (p)\t', pk.p)
+  printConsole && console.log('generator  (g)\t', pk.g)
+  printConsole && console.log('dec secret (x)\t', sk)
+  printConsole && console.log('           (h)\t', pk.h)
+  console.log('plaintext  (m)\t', message)
+  printConsole && console.log('------------------------')
+  decrypt(encrypt(message, pk), sk, pk)
+  console.log('\n')
 }
 
 // plaintext check may be wrong as it is checked against the message from above
-const m1 = new BN(2, 10);
-const e_m1 = encrypt(m1, pk);
+const m1 = new BN(2, 10)
+const e_m1 = encrypt(m1, pk)
 //const d_m1 = decrypt(e_m1, sk, p, gen);
 
-const m2 = new BN(3, 10);
-const e_m2 = encrypt(m2, pk);
+const m2 = new BN(3, 10)
+const e_m2 = encrypt(m2, pk)
 //const d_m2 = decrypt(e_m2, sk, p, gen);
 
-const d_sum = decrypt(add(e_m1, e_m2), sk, pk);
-console.log(d_sum);
+const d_sum = decrypt(add(e_m1, e_m2), sk, pk)
+console.log(d_sum)
