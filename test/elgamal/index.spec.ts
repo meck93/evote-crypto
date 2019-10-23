30043eae86b9bdd8990ead25d5697a70d08d9a69
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-10-23 09:50:35 +0200

    elgamal: test decryption methods

--- test/elgamal/index.spec.ts
+++ test/elgamal/index.spec.ts
@@ -1,9 +1,32 @@
-export {};
+export {}
+import { generateKeys, encrypt, decrypt1, decrypt2 } from '../../src/elgamal'
 
-const { expect } = require("chai");
+const BN = require('bn.js')
+const random = require('random')
+const { expect } = require('chai')
 
-describe("Simple test suite (with mocha & chai):", function() {
-  it("1 === 1 should be true", function() {
-    expect(1).to.equal(1);
-  });
-});
+describe('ElGamal Index', () => {
+  it('compare decryption implementations', () => {
+    const prnt = false
+    const [pk, sk] = generateKeys(7, 3)
+
+    const message = new BN(random.int(1, pk.p - 1), 10)
+    for (let i = 0; i < 10; i++) {
+      prnt && console.log(i)
+      prnt && console.log('prime      (p)\t', pk.p)
+      prnt && console.log('generator  (g)\t', pk.g)
+      prnt && console.log('dec secret (x)\t', sk)
+      prnt && console.log('           (h)\t', pk.h)
+      prnt && console.log('plaintext    (m)', message)
+      prnt && console.log('------------------------')
+
+      const m_enc = encrypt(message, pk, prnt)
+      const m_d1 = decrypt1(m_enc, sk, pk, prnt)
+      const m_d2 = decrypt2(m_enc, sk, pk, prnt)
+
+      expect(m_d1.eq(message)).to.be.true
+      expect(m_d2.eq(message)).to.be.true
+      expect(m_d1.eq(m_d2)).to.be.true
+    }
+  })
+})
