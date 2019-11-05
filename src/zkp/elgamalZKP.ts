60a73c5a8ff7b79491a571c2d0187a9b05d405a2
Moritz Eck <moritz.eck@gmail.com>
2019-11-05 10:28:00 +0100

    Crypto - add logging to NoProof - remove old ToDo's

--- src/zkp/elgamalZKP.ts
+++ src/zkp/elgamalZKP.ts
@@ -19,11 +19,10 @@ const invm = (a: any, pk: PublicKey) => a.invm(pk.p)
 
 // Generates a proof for an encrypted yes vote.
 export function generateYesProof(cipher: Cipher, pk: PublicKey, uniqueID: string): Proof {
-  // extract encrypted message parts
   // a = g^r, b = public_key i.e. h^r*g^m
   const { a, b, r } = cipher
 
-  // Generate fake values for m=0
+  // generate fake values for m=0
   const c0 = newBN(random.int(1, pk.q - 1))
   const f0 = newBN(random.int(1, pk.q - 1))
 
@@ -33,17 +32,15 @@ export function generateYesProof(cipher: Cipher, pk: PublicKey, uniqueID: string
   // compute fake b0. (a,b) = (a,b)
   const b0 = div(pow(pk.h, f0, pk), pow(b, c0, pk), pk)
 
-  // Generate proof for m=1
+  // generate proof for m=1
   const x = newBN(random.int(1, pk.q - 1))
 
   const a1 = pow(pk.g, x, pk)
   const b1 = pow(pk.h, x, pk)
 
-  // TODO: fix the challenge generation such that the hash function output is always valid
-  const challenge = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
-  // const challenge = newBN(random.int(1, pk.q - 1))
-
-  const c1 = add(pk.q, sub(challenge, c0, pk), pk)
+  // generate the challenge
+  const c = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
+  const c1 = add(pk.q, sub(c, c0, pk), pk)
 
   // compute f1 = x + c1 * r (NOTE: mod q!)
   const c1r = c1.mul(r).mod(pk.q)
@@ -56,7 +53,7 @@ export function generateYesProof(cipher: Cipher, pk: PublicKey, uniqueID: string
   printConsole && console.log('x\t\t\t', x.toNumber())
   printConsole && console.log('a1\t\t\t', a1.toNumber())
   printConsole && console.log('b1\t\t\t', b1.toNumber())
-  printConsole && console.log('c\t\t', challenge.toNumber())
+  printConsole && console.log('c\t\t', c.toNumber())
   printConsole && console.log('c1 = (q + (c - c0) % q) % q\t', c1.toNumber())
   printConsole && console.log('f1 = x + c1*r\t\t', f1.toNumber())
   printConsole && console.log()
@@ -69,7 +66,7 @@ export function generateNoProof(cipher: Cipher, pk: PublicKey, uniqueID: string)
   // a = g^r, b = public_key i.e. h^r*g^m
   const { a, b, r } = cipher
 
-  // Generate fake values for m=1
+  // generate fake values for m=1
   const c1 = newBN(random.int(1, pk.q - 1))
   const f1 = newBN(random.int(1, pk.q - 1))
 
@@ -82,16 +79,29 @@ export function generateNoProof(cipher: Cipher, pk: PublicKey, uniqueID: string)
   // compute fake b1
   const b1 = div(pow(pk.h, f1, pk), pow(b_, c1, pk), pk)
 
-  // Generate proof for m=0
+  // generate proof for m=0
   const x = newBN(random.int(1, pk.q - 1))
   const a0 = pow(pk.g, x, pk)
   const b0 = pow(pk.h, x, pk)
 
-  const challenge = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
-  const c0 = add(pk.q, sub(challenge, c1, pk), pk)
+  // generate the challenge
+  const c = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
+  const c0 = add(pk.q, sub(c, c1, pk), pk)
 
   // compute f0 = x + c0 * r (NOTE: mod q!)
   const f0 = add(x, c0.mul(r).mod(pk.q), pk)
+
+  printConsole && console.log('c0\t\t\t', c0.toNumber())
+  printConsole && console.log('f0\t\t\t', f0.toNumber())
+  printConsole && console.log('a0\t\t\t', a0.toNumber())
+  printConsole && console.log('b0\t\t\t', b0.toNumber())
+  printConsole && console.log('x\t\t\t', x.toNumber())
+  printConsole && console.log('a1\t\t\t', a1.toNumber())
+  printConsole && console.log('b1\t\t\t', b1.toNumber())
+  printConsole && console.log('c\t\t', c.toNumber())
+  printConsole && console.log('c1 = (q + (c - c0) % q) % q\t', c1.toNumber())
+  printConsole && console.log('f1 = x + c1*r\t\t', f1.toNumber())
+  printConsole && console.log()
 
   return { a0, a1, b0, b1, c0, c1, f0, f1 }
 }
@@ -105,7 +115,7 @@ export function verifyProof(cipher: Cipher, proof: Proof, pk: any, uniqueID: str
   const r1 = mul(a0, pow(a, c0, pk), pk)
   const v1 = l1.eq(r1)
 
-  // verification g^f1 == a1*a^c1Test
+  // verification g^f1 == a1*a^c1
   const l2 = pow(pk.g, f1, pk)
   const r2 = mul(a1, pow(a, c1, pk), pk)
   const v2 = l2.eq(r2)
@@ -115,19 +125,19 @@ export function verifyProof(cipher: Cipher, proof: Proof, pk: any, uniqueID: str
   const r3 = mul(b0, pow(b, c0, pk), pk)
   const v3 = l3.eq(r3)
 
-  // verification h^f1 == b1 * (b/g)^c1Test
+  // verification h^f1 == b1 * (b/g)^c1
   const l4 = pow(pk.h, f1, pk)
   const r4 = mul(b1, pow(div(b, pk.g, pk), c1, pk), pk)
   const v4 = l4.eq(r4)
 
   // recompute the hash and verify
-  const c = c1.add(c0).mod(pk.q)
+  const lc = c1.add(c0).mod(pk.q)
 
-  const challenge = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
-  const v5 = challenge.eq(c)
+  const rc = generateChallenge(pk.q, uniqueID, a, b, a0, b0, a1, b1)
+  const v5 = lc.eq(rc)
 
   printConsole && console.log('g^f0 == a0*a^c0:\t', v1)
-  printConsole && console.log('g^f1 == a1*a^c1Test\t', v2)
+  printConsole && console.log('g^f1 == a1*a^c1\t', v2)
   printConsole && console.log('h^f0 == b0*b^c0\t\t', v3)
   printConsole && console.log('h^f1 == b1*(b/g)^c1\t', v4)
   printConsole && console.log('c == c1 + c0\t\t', v5)
