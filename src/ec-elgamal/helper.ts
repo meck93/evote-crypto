649125c2429da7f173edb51d1eeda0301e092195
Moritz Eck <moritz.eck@gmail.com>
2019-11-06 12:33:34 +0100

    Crypto - ECC: Fix value size check in helper method -> getSecureRandom()

--- src/ec-elgamal/helper.ts
+++ src/ec-elgamal/helper.ts
@@ -2,19 +2,19 @@ import crypto = require('crypto')
 
 const BN = require('bn.js')
 const EC = require('elliptic').ec
-const ec = new EC('secp256k1')
+const secp256k1 = new EC('secp256k1')
 
 const RAND_SIZE_BYTES = 32
-const UPPER_BOUND_RANDOM = ec.curve.n.sub(new BN(2, 10))
+const UPPER_BOUND_RANDOM = secp256k1.curve.n.sub(new BN(1, 10))
 
 export const getSecureRandomValue = (): any => {
-    let randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
-    let randomValue = new BN(randomBytes)
-  
-    // ensure that the random value is in range [1,n-1]
-    while (!randomValue.lte(UPPER_BOUND_RANDOM) && randomValue.gte(1)) {
-      randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
-      randomValue = new BN(randomBytes, 'hex')
-    }
-    return randomValue
+  let randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
+  let randomValue = new BN(randomBytes, 'hex')
+
+  // ensure that the random value is in range [1,n-1]
+  while (!(randomValue.lte(UPPER_BOUND_RANDOM) && randomValue.gte(1))) {
+    randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
+    randomValue = new BN(randomBytes, 'hex')
   }
+  return randomValue
+}
