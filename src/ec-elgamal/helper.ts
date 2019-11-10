0622ef27e99a939c181ca930b2dac8b94309f7e2
nikzaugg <zauggnik@gmail.com>
2019-11-10 10:32:20 +0100

    crypto - add BN type to secure random value

--- src/ec-elgamal/helper.ts
+++ src/ec-elgamal/helper.ts
@@ -1,18 +1,18 @@
 import crypto = require('crypto')
 
-const BN = require('bn.js')
+import BN = require('bn.js')
 const EC = require('elliptic').ec
 const secp256k1 = new EC('secp256k1')
 
 const RAND_SIZE_BYTES = 32
 const UPPER_BOUND_RANDOM = secp256k1.curve.n.sub(new BN(1, 10))
 
-export const getSecureRandomValue = (): any => {
+export const getSecureRandomValue = (): BN => {
   let randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
   let randomValue = new BN(randomBytes, 'hex')
 
   // ensure that the random value is in range [1,n-1]
-  while (!(randomValue.lte(UPPER_BOUND_RANDOM) && randomValue.gte(1))) {
+  while (!(randomValue.lte(UPPER_BOUND_RANDOM) && randomValue.gte(new BN(1)))) {
     randomBytes = crypto.randomBytes(RAND_SIZE_BYTES)
     randomValue = new BN(randomBytes, 'hex')
   }
