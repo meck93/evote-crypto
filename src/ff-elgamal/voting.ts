7317663f768e9821fc0c0f093fc3491a329733de
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2019-11-19 13:45:22 +0100

    crypto - modify addVote to use a base vote - every call to addVote returns the same "sum" vote as no random value is used

--- src/ff-elgamal/voting.ts
+++ src/ff-elgamal/voting.ts
@@ -1,17 +1,14 @@
 import { encrypt, add, decrypt1 } from './encryption'
 import { PublicKey } from './models'
 import { Cipher } from '../models'
+import { newBN } from './helper'
 
-export const generateYesVote = (pk: PublicKey): Cipher => {
-  return encrypt(1, pk)
-}
-
-export const generateNoVote = (pk: PublicKey): Cipher => {
-  return encrypt(0, pk)
-}
+export const generateYesVote = (pk: PublicKey): Cipher => encrypt(1, pk)
+export const generateNoVote = (pk: PublicKey): Cipher => encrypt(0, pk)
+export const generateBaseVote = (): Cipher => ({ a: newBN(1), b: newBN(1) }) // encrypt with m=0, r=0
 
 export const addVotes = (votes: Cipher[], pk: PublicKey): Cipher => {
-  return votes.reduce((previous, current) => add(previous, current, pk), generateNoVote(pk))
+  return votes.reduce((previous, current) => add(previous, current, pk), generateBaseVote())
 }
 
 export const tallyVotes = (pk: PublicKey, sk: any, votes: Cipher[]): number => {
