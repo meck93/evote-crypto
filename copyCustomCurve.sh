993a3fb7f9cb93b83202448c1d12e63662864a5e
Moritz Eck <moritz.eck@gmail.com>
2019-12-17 14:51:29 +0100

    Crypto: Package
    - removed two scripts (local & production) -> replaced with one single script which checks the installation paths
    - add: elliptic as bundledDependencies
    - bump version: 0.1.5

---
+++ copyCustomCurve.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# this script is run after each npm install
+# please see package.json -> scripts -> postinstall
+
+# get relative paths, so that this also works when called from
+# outisde this directory
+
+readonly name=$(basename $0)
+readonly dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly parentDir="$(dirname "$dir")"
+readonly parentParentDir="$(dirname "$parentDir")"
+
+# replace curve file inside elliptic package
+# this adds the elliptic curve. curve25519 in weierstrass form to the elliptic library
+# since this pull request is not merged yet: https://github.com/indutny/elliptic/pull/113
+fileToCopy=$dir/curves.js
+
+# top level node_modules (e.g., node_modules/elliptic)
+topLevelDest=$parentParentDir/elliptic/lib/elliptic/curves.js
+
+# node_modules inside @meck93/evote-crypto -> bundled dependencies (e.g., node_modules/@meck93/evote-crypto/node_modules/elliptic)
+dependencyDest=$dir/node_modules/elliptic/lib/elliptic/curves.js
+
+# check if the top level destination exist -> if yes, copy the custom curve file
+[ -f $topLevelDest ] && { echo "$topLevelDest exist"; cp $fileToCopy $topLevelDest; }
+
+# check if the top level destination exist -> if yes, copy the custom curve file
+[ -f $dependencyDest ] && { echo "$dependencyDest exist"; cp $fileToCopy $dependencyDest; }
